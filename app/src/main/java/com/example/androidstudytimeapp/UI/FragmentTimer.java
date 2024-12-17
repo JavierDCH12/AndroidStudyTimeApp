97efdd48a4a141119dcd3f87607e62d8da28c244
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-12-17 17:42:34 +0100

    timer fragment added

---
+++ app/src/main/java/com/example/androidstudytimeapp/UI/FragmentTimer.java
@@ -0,0 +1,43 @@
+package com.example.androidstudytimeapp.UI;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidstudytimeapp.R;
+import com.example.androidstudytimeapp.databinding.FragmentTimerBinding;
+
+
+public class FragmentTimer extends Fragment {
+
+    FragmentTimerBinding binding;
+    public FragmentTimer() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentTimerBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
