fc7a1b780236784f36738b4af76efc576085e287
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-12-17 17:31:08 +0100

    first commit

---
+++ app/src/main/java/com/example/androidstudytimeapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.androidstudytimeapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.androidstudytimeapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }//ONCREATE END
+
+
+}//MAIN END
